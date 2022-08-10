b6c45bf225e458d11f59cb398b991d662990cef8
Harshavardhan Amula <harshavardhan.amula@valuemomentum.com>
2022-08-10 12:26:06 +0530

    bookapp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BookApp'`, () => {
+  it(`should have as title 'bookapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BookApp');
+    expect(app.title).toEqual('bookapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BookApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bookapp app is running!');
   });
 });
